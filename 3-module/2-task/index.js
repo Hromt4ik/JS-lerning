189b248d3bff2285dc9ecb7d6205bc33a09225cb
Hromt4ik <97237642+Hromt4ik@users.noreply.github.com>
2023-02-08 13:25:54 +0500

    Update index.js

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,9 +1,3 @@
 function filterRange(arr, a, b) {
-  let arr2 = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= a && arr[i] <= b) {
-      arr2.push(arr[i]);
-    }
-  }
-  return arr2;
+  return arr.filter(item => item >= a && item <= b);
 }
